a6e6305530aef574d52c254514fa244f9ac98e87
taghreed2 <taghreed.mohammed.ruh.s@tuwaiq.edu.sa>
2022-03-08 14:45:43 +0300

    i

---
+++ code/code/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  code
+//
+//  Created by TAGHREED on 05/08/1443 AH.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func arry(arr:[Int]) -> Int {
+    var x = 0
+    
+    for i in arr {
+        for j in arr {
+           
+            if j == i {
+                
+            }
+        }
+        
+    }
+    
+    return x
+}
+
+//func strg(str : String) -> String {
+//    var str1 : String
+//
+//    for i in str {
+//        if i.isUppercase {
+//            str1.append(contentsOf:  i.lowercased())
+//
+//        }else if i.isLowercase{
+//            str1.append(contentsOf:  i.uppercased())
+//        }
+//    }
+//    return str1
+//}
+
+
