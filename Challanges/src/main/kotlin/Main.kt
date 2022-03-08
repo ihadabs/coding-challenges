ddc8a7d7e1c9eb1dd957749195f4bdaf9060f23a
Dalia Alzahrani <dalmashhaf@hotmail.com>
2022-03-08 14:47:59 +0300

    Challenge

---
+++ Challanges/src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+import java.util.*
+
+//1st Challenge
+//Create a function that receives an integers array and returns the number that appeared only once.
+fun main() {
+val array: ArrayList<Int> = (1,2,3,4)
+}
+/***********************************************/
+//2st Challenge
+//Create a function that receives a string then it converts uppercase letters into lowercase and vice versa.
+// The function then should print the converted value.
+fun main(){
+    var s:String = listOf().readLine
+    s.toString("hELLO")
+
+}
+/***********************************************/
+//3st Challenge
+//Create a function that receives an array of items & arrays and returns
+// one flattened array with all items exluding null values.
+fun main(){
+var s = readLine.ArrayList<T>()
+
+}
+/***********************************************/
+//4st Challenge
+//Create a function that receives a string that contains combination of parentheses, square brackets,
+// and curly braces. Then, it returns true if every opening bracket has a closing pair.
+fun checker():Boolean{
+
+    var text = "(Hello world)"
+    if (text=="()"){
+        return true
+    }
+    else false
+}
