44aa6268d8204d8ab63a34099f941431f3cb4938
MonafhTM <eng.mt7@hotmail.com>
2022-03-08 16:52:25 +0300

    Monafh

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+// 1st Challenge .....
+
+func printNum(array : [Int]) -> Int {
+    
+    var i = 0
+    
+    
+    return i
+    
+}
+
+var first = printNum(array: [2,4,6,4,9,6,2])
+var secnd = printNum(array: [1,1,2])
+var thir = printNum(array: [2,2,1])
+print(first)
+print(secnd)
+print(thir)
+
+//2nd Challenge ..
+
+func upperAndLower (name : String) -> String {
+    
+    var newname = ""
+    
+    return newname
+    
+}
+var printName = upperAndLower(name: "Hello")
+var printsecName = upperAndLower(name: "Hi")
+var printthirName = upperAndLower(name: "Hello World")
+var printfourName = upperAndLower(name: "My name is Ali")
+var printfifName = upperAndLower(name: "sHroog")
+
+// 3rd Challenge ...
+
+func flattenedArray(items : Any) -> [Int]{
+    
+    
+    return
+}
+
+
+// 4rd Challenge ...
+
+func sympls(parentheses : String) -> Bool {
+    
+    
+}
+
+
